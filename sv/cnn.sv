653a57926b54ea89a66189f73b1ae601eb7d042c
Andrew P <andrewlpan@gmail.com>
2025-02-18 09:27:45 -0800

    changed logic in assigning outputs

--- sv/cnn.sv
+++ sv/cnn.sv
@@ -11,6 +11,7 @@ module cnn
     ,input shortreal weights_i[M_p][N_p][K_p][K_p]
     ,input logic clk_i
     ,input logic reset_i
+    ,input logic valid_i
     ,output shortreal fm_o[M_p][R_p][C_p]
     );
 
@@ -20,12 +21,13 @@ module cnn
     int to, ti;
     int i, j;
     logic pulse_j, pulse_i, pulse_ti, pulse_to, pulse_col, pulse_row;
+    logic done;
 
     cnn_counter #(.max_p( K_p ), .stride_p ( 1 ))
         j_counter
         (.reset_i(reset_i)
         ,.clk_i(clk_i)
-        ,.en_i(~reset_i)
+        ,.en_i(( ~reset_i ) & ( ~done ))
         ,.pulse_o(pulse_j)
         ,.it_o(j)
         );
@@ -75,44 +77,82 @@ module cnn
         ,.it_o(row)
         );
 
-    /*
+    // FSM for state control
+    // TODO: change typedef
+    typedef enum {eWAIT, eBUSY, eDONE} state_e;
+    state_e state_p, state_n;
+
+    // next state logic
+    always_comb begin
+        case(state_p)
+            eWAIT: if (valid_i) state_n = eBUSY;
+                else state_n = eWAIT;
+            eBUSY: if (pulse_row) state_n = eDONE;
+                else state_n = eBUSY;
+            eDONE: state_n = eDONE;
+        endcase
+    end
+
+    assign done = (state_p == eDONE);
+
+    // store state for state machine, reset to eWAIT
+    always_ff @(posedge clk_i) begin
+        if (reset_i) state_p <= eWAIT;
+        else state_p <= state_n;
+    end
+
+    
     // ******** DATAPATH ********
     shortreal weights_lo[Tm_p][Tn_p]; // fed into outloop
     shortreal fm_lo[Tn_p]; 
-    shortreal fm_reg_o[Tm_p][R_p][C_p];
+    // shortreal fm_reg_o[Tm_p][R_p][C_p];
     shortreal fm_reg_n[Tm_p]; // output from loop module
 
     // generate vector for fm_lo
     genvar k;
     generate
-        for (k = 0; k < Tn_p) begin
+        for (k = 0; k < Tn_p; k++) begin
             assign fm_lo[k] = fm_i[k][i][k];
         end
     endgenerate
 
     // generate vector for weights_lo
-    
+    // hardcode for now TODO: fix
     genvar x;
     generate
-        for (x = 0; x < Tm_p; x++) begin
-            for (int y = 0; y < Tn_p; y++) begin
-                assign weights_lo[x][y] = weights_i[x][y][i][j];
-            end
+        for (x = 0; x < Tn_p; x++) begin
+            assign weights_lo[0][x] = weights_i[0][x][i][j];
         end
     endgenerate
 
-    output_loop #(.Tm_p( Tm_p ), Tn_p( Tn_p ))
+    genvar y;
+    generate
+        for (y = 0; y < Tn_p; y++) begin
+            assign weights_lo[1][y] = weights_i[1][y][i][j];
+        end
+    endgenerate
+
+    shortreal init_test[2] = {0, 0};
+
+    output_loop #(.Tm_p( Tm_p ), .Tn_p( Tn_p ))
         loop 
         (.weights_i( weights_lo )
         ,.fm_i( fm_lo )
-        ,.fm_init_i ( '{default:0} )
+        ,.fm_init_i ( init_test )
         ,.fm_o ( fm_reg_n )
         );
 
     // store output in reg
+    // reset all values to 0
     always_ff @( clk_i ) begin
         if (reset_i) begin
-            fm_reg_o <= '{default:0};
+            for (int i = 0; i < N_p; i++) begin
+                for (int j = 0; j < R_p; j++) begin
+                    for (int k = 0; k < C_p; k++) begin
+                        fm_o[i][j][k] <= 0;
+                    end
+                end
+            end
         end
     end
     // update corresponding output fm
@@ -120,11 +160,9 @@ module cnn
     generate
         for (z = 0; z < Tm_p; z++) begin
             always_ff @( clk_i) begin
-                fm_reg_o[z][row][col] <= fm_reg_n[z];
+                fm_o[to + z][row][col] <= fm_reg_n[z] + fm_o[to + z][row][col];
             end
         end
     endgenerate
     
-    assign fm_o = fm_reg_o;
-    */
 endmodule
--- sv/cnn_testbench.sv
+++ sv/cnn_testbench.sv
@@ -1,10 +1,32 @@
 module cnn_testbench ();
     parameter N_p=4, M_p=4, K_p=2, R_p=16, C_p=16, S_p=1, Tn_p=2, Tm_p=2;
 
-    shortreal fm_i[N_p][R_p][C_p] = '{default:0};
-    shortreal weights_i[M_p][N_p][K_p][K_p] = '{default:0};
+    shortreal fm_i[N_p][R_p][C_p];
+    shortreal weights_i[M_p][N_p][K_p][K_p];
     shortreal fm_o[M_p][R_p][C_p];
-    logic clk_i, reset_i;
+    logic clk_i, reset_i, valid_i;
+
+    initial begin
+        for (int i = 0; i < N_p; i++) begin
+            for (int j = 0; j < R_p; j++) begin
+                for (int k = 0; k < C_p; k++) begin
+                    fm_i[i][j][k] = shortreal'($urandom_range(0, 1000000)) / 1000000.0;
+                end
+            end
+        end
+    end
+
+    initial begin
+        for (int x = 0; x < M_p; x++) begin
+            for (int i = 0; i < N_p; i++) begin
+                for (int j = 0; j < K_p; j++) begin
+                    for (int k = 0; k < K_p; k++) begin
+                        weights_i[x][i][j][k] = 1;
+                    end
+                end
+            end
+        end
+    end
 
     cnn #(.N_p( 4 ), .M_p( 4 ), .K_p( 2 ), .R_p( 16 )
         ,.C_p( 16 ), .S_p( 1 ), .Tn_p ( 2 ), .Tm_p( 2 ) )
@@ -13,6 +35,7 @@ module cnn_testbench ();
         ,weights_i
         ,clk_i
         ,reset_i
+        ,valid_i
         ,fm_o
         );
 
@@ -28,7 +51,9 @@ module cnn_testbench ();
         reset_i <= 0; @(posedge clk_i);
         reset_i <= 1; @(posedge clk_i);
         reset_i <= 0; @(posedge clk_i);
-        repeat(100) @(posedge clk_i);
+        valid_i <= 1; @(posedge clk_i);
+        valid_i <= 0; @(posedge clk_i);
+        repeat(300) @(posedge clk_i);
         $stop;
 
     end
