a03437c43e6ff5e1222097e73eec892627bb314f
Andrew P <andrewlpan@gmail.com>
2025-02-18 00:38:18 -0800

    start writing datapath, need to think about weights_lo

--- sv/cnn.sv
+++ sv/cnn.sv
@@ -9,19 +9,68 @@ module cnn
 
     (input shortreal fm_i[N_p][R_p][C_p]
     ,input shortreal weights_i[M_p][N_p][K_p][K_p]
-    ,input logic clk
-    ,input logic reset
+    ,input logic clk_i
+    ,input logic reset_i
     ,output shortreal fm_o[M_p][R_p][C_p]
     );
 
+    // ******** CONTROL ********
+    // instantiate iterator counters
     int row, col;
     int i, j;
+    logic pulse_j, pulse_i, pulse_col, pulse_row;
+    cnn_counter (.max_p( K_p ))
+        j_counter
+        (.reset_i(reset_i)
+        ,.clk_i(clk_i)
+        ,.en_i(~reset_i)
+        ,.pulse_o(pulse_j)
+        ,.it_o(j)
+        );
+    
+    cnn_counter (.max_p( K_p ))
+        i_counter
+        (.reset_i(reset_i)
+        ,.clk_i(clk_i)
+        ,.en_i(pulse_j)
+        ,.pulse_o(pulse_i)
+        ,.it_o(i)
+        );
+    
+    cnn_counter (.max_p( K_p ))
+        col_counter
+        (.reset_i(reset_i)
+        ,.clk_i(clk_i)
+        ,.en_i(pulse_i)
+        ,.pulse_o(pulse_col)
+        ,.it_o(col)
+        );
+    
+    cnn_counter (.max_p( K_p ))
+        row_counter
+        (.reset_i(reset_i)
+        ,.clk_i(clk_i)
+        ,.en_i(pulse_col)
+        ,.pulse_o(pulse_row)
+        ,.it_o(row)
+        );
 
+    // ******** DATAPATH ********
     shortreal weights_lo[Tm_p][Tn_p]; // fed into outloop
     shortreal fm_lo[Tn_p]; 
 
     assign fm_lo[0] = {tii, tii+1, tii+2, tii+3};
     assign fm_lo[1]
+
+    // generate vector for fm_lo
+    genvar k
+    generate
+        for (k = 0; k < Tn_p) begin
+            assign fm_lo[k] = fm_i[k][i][k];
+        end
+    endgenerate
+
+    // generate vector for weights_lo
 
     module #(.Tm_p( Tm_p ), Tn_p( Tn_p ))
         loop 
